e694b37229a3e6c8e70d225bcb1ff9282c0bce96
Bulent Turkmen <trkmen.blent@gmail.com>
2021-08-25 14:58:07 +0300

    deleted homedatasource interface

--- app/src/main/java/com/faranjit/meditory/features/home/data/datasource/HomeRemoteDataSource.kt
+++ app/src/main/java/com/faranjit/meditory/features/home/data/datasource/HomeRemoteDataSource.kt
@@ -4,7 +4,6 @@ import com.faranjit.meditory.base.Executor
 import com.faranjit.meditory.base.ResponseWrapper
 import com.faranjit.meditory.features.home.data.HomeApi
 import com.faranjit.meditory.features.home.data.response.HomeResponse
-import com.faranjit.meditory.features.home.domain.HomeDataSource
 
 /**
  * Created by Bulent Turkmen on 25.08.2021.
@@ -12,9 +11,9 @@ import com.faranjit.meditory.features.home.domain.HomeDataSource
 class HomeRemoteDataSource(
     private val homeApi: HomeApi,
     private val executor: Executor
-) : HomeDataSource {
+) {
 
-    override suspend fun getHomeData(): ResponseWrapper<HomeResponse> =
+    suspend fun getHomeData(): ResponseWrapper<HomeResponse> =
         executor.call {
             homeApi.getHomeData()
         }
